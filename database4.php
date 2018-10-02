2c85d49ceae1b8dd87f478ab95a5f4ba7abaa52b
palmerhinchliffe <palmerhinchliffe@outlook.com>
2018-10-02 20:30:05 +1000

    improved-security

--- database4.php
+++ database4.php
@@ -4,6 +4,21 @@ include 'db.php';
 $id=$_GET['eid'];
 $name=$_GET['ename'];
 
+/* Using PDO (prepared statements) prevents SQL injection: */
+$sql = $conn->prepare("INSERT INTO emp (eid, ename) VALUES (?, ?)");
+$sql->bind_param("is", $id, $name);
+$result = $sql->execute();
+
+if (!$result) {
+	echo "Error inserting data";
+	$sql->close();
+} else {
+	 echo "Data inserted successfully";
+	 $sql->close();
+ }
+
+/* Prone to SQL Injection:
+
 $sql="INSERT INTO `emp` (`eid`,`ename`) values ('$id','$name')";
 $check=mysql_query($sql,$conn);
 if(!$check)
@@ -11,4 +26,5 @@ if(!$check)
 else echo "Data Inserted Succesfully";
 
 mysql_close($conn);
+*/
 ?>
